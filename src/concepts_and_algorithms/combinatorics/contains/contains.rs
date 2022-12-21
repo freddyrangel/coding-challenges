78e9cb1558c823b6b52a1bac3024dbb332f27ebb
Freddy Rangel <frederick.rangel@gmail.com>
2022-12-21 06:45:31 +0100

    Remove comments and add description to contains.rs

--- src/concepts_and_algorithms/combinatorics/contains/contains.rs
+++ src/concepts_and_algorithms/combinatorics/contains/contains.rs
@@ -1,7 +1,7 @@
 /*
  * CONTAINED IN ARRAY
  * https://www.interviewcake.com/question/javascript/find-in-ordered-set
- * Check if an integer is present in the array
+ * Check if an integer is present in a sorted array
  */
 
 #[cfg(test)]
@@ -56,6 +56,7 @@ mod tests {
     // let expected = false;
     // assert_eq!(actual, expected);
     // }
+
     // #[test]
     // fn large_array_number_first() {
     // let actual = contains(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1);
