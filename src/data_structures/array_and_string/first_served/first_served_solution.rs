f4d52be1e1fb9cb2e86d0f9eda5c37fad858f222
Freddy Rangel <frederick.rangel@gmail.com>
2023-04-20 12:48:28 +0200

    Updated solution for first_served

--- src/data_structures/array_and_string/first_served/first_served_solution.rs
+++ src/data_structures/array_and_string/first_served/first_served_solution.rs
@@ -4,27 +4,33 @@
  * Check if we're serving orders first-come, first-served
  */
 
-fn is_first_come_first_served(
-    take_out: Vec<i32>,
-    dine_in: Vec<i32>,
-    served_orders: Vec<i32>,
-) -> bool {
-    if served_orders.len() != take_out.len() + dine_in.len() {
-        false
-    } else {
-        check_orders(&take_out, &served_orders) && check_orders(&dine_in, &served_orders)
-    }
-}
+fn is_first_come_first_served(take_out: &[i32], dine_in: &[i32], served: &[i32]) -> bool {
+    let mut take_out_index = 0;
+    let mut dine_in_index = 0;
+    let take_out_max_index = take_out.len();
+    let dine_in_max_index = dine_in.len();
 
-fn check_orders(orders: &Vec<i32>, served_orders: &Vec<i32>) -> bool {
-    let temp = served_orders.into_iter().fold(vec![], |mut result, order| {
-        if orders.contains(&order) {
-            result.push(*order);
+    for order in served {
+        if take_out_index < take_out_max_index
+            && order == &take_out[take_out_index]
+        {
+            take_out_index += 1;
+        } else if dine_in_index < dine_in_max_index
+            && order == &dine_in[dine_in_index]
+        {
+            dine_in_index += 1;
+        } else {
+            return false;
         }
-        result
-    });
+    }
 
-    temp == *orders
+    if dine_in_index != dine_in.len()
+        || take_out_index != take_out.len()
+    {
+        return false;
+    }
+
+    true
 }
 
 #[cfg(test)]
@@ -34,52 +40,52 @@ mod tests {
     #[test]
     fn both_registers_have_same_number_of_orders() {
         let actual =
-            is_first_come_first_served(vec![1, 4, 5], vec![2, 3, 6], vec![1, 2, 3, 4, 5, 6]);
+            is_first_come_first_served(&vec![1, 4, 5], &vec![2, 3, 6], &vec![1, 2, 3, 4, 5, 6]);
         assert_eq!(actual, true);
     }
 
     #[test]
     fn registers_have_different_lengths() {
-        let actual = is_first_come_first_served(vec![1, 5], vec![2, 3, 6], vec![1, 2, 6, 3, 5]);
+        let actual = is_first_come_first_served(&vec![1, 5], &vec![2, 3, 6], &vec![1, 2, 6, 3, 5]);
         assert_eq!(actual, false);
     }
 
     #[test]
     fn one_register_is_empty() {
-        let actual = is_first_come_first_served(vec![], vec![2, 3, 6], vec![2, 3, 6]);
+        let actual = is_first_come_first_served(&vec![], &vec![2, 3, 6], &vec![2, 3, 6]);
         assert_eq!(actual, true);
     }
 
     #[test]
     fn served_orders_is_missing_orders() {
-        let actual = is_first_come_first_served(vec![1, 5], vec![2, 3, 6], vec![1, 6, 3, 5]);
+        let actual = is_first_come_first_served(&vec![1, 5], &vec![2, 3, 6], &vec![1, 6, 3, 5]);
         assert_eq!(actual, false);
     }
 
     #[test]
     fn served_orders_has_extra_orders() {
-        let actual = is_first_come_first_served(vec![1, 5], vec![2, 3, 6], vec![1, 2, 3, 5, 6, 8]);
+        let actual = is_first_come_first_served(&vec![1, 5], &vec![2, 3, 6], &vec![1, 2, 3, 5, 6, 8]);
         assert_eq!(actual, false);
     }
 
     #[test]
     fn one_register_has_extra_orders() {
-        let actual = is_first_come_first_served(vec![1, 9], vec![7, 8], vec![1, 7, 8]);
+        let actual = is_first_come_first_served(&vec![1, 9], &vec![7, 8], &vec![1, 7, 8]);
         assert_eq!(actual, false);
     }
 
     #[test]
     fn one_register_has_unserved_orders() {
-        let actual = is_first_come_first_served(vec![55, 9], vec![7, 8], vec![1, 7, 8, 9]);
+        let actual = is_first_come_first_served(&vec![55, 9], &vec![7, 8], &vec![1, 7, 8, 9]);
         assert_eq!(actual, false);
     }
 
     #[test]
     fn order_numbers_are_not_sequential() {
         let actual = is_first_come_first_served(
-            vec![27, 12, 18],
-            vec![55, 31, 8],
-            vec![55, 31, 8, 27, 12, 18],
+            &vec![27, 12, 18],
+            &vec![55, 31, 8],
+            &vec![55, 31, 8, 27, 12, 18],
         );
         assert_eq!(actual, true);
     }
