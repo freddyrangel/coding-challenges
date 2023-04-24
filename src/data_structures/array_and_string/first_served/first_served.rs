65089746e155b94a9f5f34018a4f6e60812b3def
Freddy Rangel <frederick.rangel@gmail.com>
2023-04-24 10:43:15 +0200

    Improve first_served even further
    I learned slices are much more versatile than specifying vectors. I also
    improved the style of the solution to use Rust's Option type for getting
    elements in a list. Let's use Rust's super powers.

--- src/data_structures/array_and_string/first_served/first_served.rs
+++ src/data_structures/array_and_string/first_served/first_served.rs
@@ -10,53 +10,52 @@ mod tests {
 
     // #[test]
     // fn both_registers_have_same_number_of_orders() {
-        // let actual =
-            // is_first_come_first_served(&vec![1, 4, 5], &vec![2, 3, 6], &vec![1, 2, 3, 4, 5, 6]);
+        // let actual = is_first_come_first_served(&[1, 4, 5], &[2, 3, 6], &[1, 2, 3, 4, 5, 6]);
         // assert_eq!(actual, true);
     // }
 
     // #[test]
     // fn registers_have_different_lengths() {
-        // let actual = is_first_come_first_served(&vec![1, 5], &vec![2, 3, 6], &vec![1, 2, 6, 3, 5]);
+        // let actual = is_first_come_first_served(&[1, 5], &[2, 3, 6], &[1, 2, 6, 3, 5]);
         // assert_eq!(actual, false);
     // }
 
     // #[test]
     // fn one_register_is_empty() {
-        // let actual = is_first_come_first_served(&vec![], &vec![2, 3, 6], &vec![2, 3, 6]);
+        // let actual = is_first_come_first_served(&[], &[2, 3, 6], &[2, 3, 6]);
         // assert_eq!(actual, true);
     // }
 
     // #[test]
     // fn served_orders_is_missing_orders() {
-        // let actual = is_first_come_first_served(&vec![1, 5], &vec![2, 3, 6], &vec![1, 6, 3, 5]);
+        // let actual = is_first_come_first_served(&[1, 5], &[2, 3, 6], &[1, 6, 3, 5]);
         // assert_eq!(actual, false);
     // }
 
     // #[test]
     // fn served_orders_has_extra_orders() {
-        // let actual = is_first_come_first_served(&vec![1, 5], &vec![2, 3, 6], &vec![1, 2, 3, 5, 6, 8]);
+        // let actual = is_first_come_first_served(&[1, 5], &[2, 3, 6], &[1, 2, 3, 5, 6, 8]);
         // assert_eq!(actual, false);
     // }
 
     // #[test]
     // fn one_register_has_extra_orders() {
-        // let actual = is_first_come_first_served(&vec![1, 9], &vec![7, 8], &vec![1, 7, 8]);
+        // let actual = is_first_come_first_served(&[1, 9], &[7, 8], &[1, 7, 8]);
         // assert_eq!(actual, false);
     // }
 
     // #[test]
     // fn one_register_has_unserved_orders() {
-        // let actual = is_first_come_first_served(&vec![55, 9], &vec![7, 8], &vec![1, 7, 8, 9]);
+        // let actual = is_first_come_first_served(&[55, 9], &[7, 8], &[1, 7, 8, 9]);
         // assert_eq!(actual, false);
     // }
 
     // #[test]
     // fn order_numbers_are_not_sequential() {
         // let actual = is_first_come_first_served(
-            // &vec![27, 12, 18],
-            // &vec![55, 31, 8],
-            // &vec![55, 31, 8, 27, 12, 18],
+            // &[27, 12, 18],
+            // &[55, 31, 8],
+            // &[55, 31, 8, 27, 12, 18],
         // );
         // assert_eq!(actual, true);
     // }
--- src/data_structures/array_and_string/first_served/first_served_solution.rs
+++ src/data_structures/array_and_string/first_served/first_served_solution.rs
@@ -7,30 +7,27 @@
 fn is_first_come_first_served(take_out: &[i32], dine_in: &[i32], served: &[i32]) -> bool {
     let mut take_out_index = 0;
     let mut dine_in_index = 0;
-    let take_out_max_index = take_out.len();
-    let dine_in_max_index = dine_in.len();
 
-    for order in served {
-        if take_out_index < take_out_max_index
-            && order == &take_out[take_out_index]
-        {
-            take_out_index += 1;
-        } else if dine_in_index < dine_in_max_index
-            && order == &dine_in[dine_in_index]
-        {
-            dine_in_index += 1;
-        } else {
-            return false;
+    for served_order in served {
+        if let Some(take_out_order) = take_out.get(take_out_index) {
+            if take_out_order == served_order {
+                take_out_index += 1;
+                continue;
+            }
         }
-    }
 
-    if dine_in_index != dine_in.len()
-        || take_out_index != take_out.len()
-    {
+        if let Some(dine_in_order) = dine_in.get(dine_in_index) {
+            if dine_in_order == served_order {
+                dine_in_index += 1;
+                continue;
+            }
+        }
+
         return false;
+
     }
 
-    true
+    take_out_index == take_out.len() && dine_in_index == dine_in.len()
 }
 
 #[cfg(test)]
@@ -39,53 +36,52 @@ mod tests {
 
     #[test]
     fn both_registers_have_same_number_of_orders() {
-        let actual =
-            is_first_come_first_served(&vec![1, 4, 5], &vec![2, 3, 6], &vec![1, 2, 3, 4, 5, 6]);
+        let actual = is_first_come_first_served(&[1, 4, 5], &[2, 3, 6], &[1, 2, 3, 4, 5, 6]);
         assert_eq!(actual, true);
     }
 
     #[test]
     fn registers_have_different_lengths() {
-        let actual = is_first_come_first_served(&vec![1, 5], &vec![2, 3, 6], &vec![1, 2, 6, 3, 5]);
+        let actual = is_first_come_first_served(&[1, 5], &[2, 3, 6], &[1, 2, 6, 3, 5]);
         assert_eq!(actual, false);
     }
 
     #[test]
     fn one_register_is_empty() {
-        let actual = is_first_come_first_served(&vec![], &vec![2, 3, 6], &vec![2, 3, 6]);
+        let actual = is_first_come_first_served(&[], &[2, 3, 6], &[2, 3, 6]);
         assert_eq!(actual, true);
     }
 
     #[test]
     fn served_orders_is_missing_orders() {
-        let actual = is_first_come_first_served(&vec![1, 5], &vec![2, 3, 6], &vec![1, 6, 3, 5]);
+        let actual = is_first_come_first_served(&[1, 5], &[2, 3, 6], &[1, 6, 3, 5]);
         assert_eq!(actual, false);
     }
 
     #[test]
     fn served_orders_has_extra_orders() {
-        let actual = is_first_come_first_served(&vec![1, 5], &vec![2, 3, 6], &vec![1, 2, 3, 5, 6, 8]);
+        let actual = is_first_come_first_served(&[1, 5], &[2, 3, 6], &[1, 2, 3, 5, 6, 8]);
         assert_eq!(actual, false);
     }
 
     #[test]
     fn one_register_has_extra_orders() {
-        let actual = is_first_come_first_served(&vec![1, 9], &vec![7, 8], &vec![1, 7, 8]);
+        let actual = is_first_come_first_served(&[1, 9], &[7, 8], &[1, 7, 8]);
         assert_eq!(actual, false);
     }
 
     #[test]
     fn one_register_has_unserved_orders() {
-        let actual = is_first_come_first_served(&vec![55, 9], &vec![7, 8], &vec![1, 7, 8, 9]);
+        let actual = is_first_come_first_served(&[55, 9], &[7, 8], &[1, 7, 8, 9]);
         assert_eq!(actual, false);
     }
 
     #[test]
     fn order_numbers_are_not_sequential() {
         let actual = is_first_come_first_served(
-            &vec![27, 12, 18],
-            &vec![55, 31, 8],
-            &vec![55, 31, 8, 27, 12, 18],
+            &[27, 12, 18],
+            &[55, 31, 8],
+            &[55, 31, 8, 27, 12, 18],
         );
         assert_eq!(actual, true);
     }
