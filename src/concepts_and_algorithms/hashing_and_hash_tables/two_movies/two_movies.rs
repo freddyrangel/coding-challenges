08dcdfcd2e362b07383f54337a787ee0ad63a422
Freddy Rangel <frederick.rangel@gmail.com>
2023-04-25 12:52:12 +0200

    Improve two_movies tests and solution

--- src/concepts_and_algorithms/hashing_and_hash_tables/two_movies/two_movies.rs
+++ src/concepts_and_algorithms/hashing_and_hash_tables/two_movies/two_movies.rs
@@ -6,10 +6,10 @@
  * movie_lengths (in minutes) and returns a boolean indicating whether there are two numbers in
  * movie_lengths whose sum equals flightLength.
  *
- * When building your function:
- *  Assume your users will watch exactly two movies
- *  Don't make your users watch the same movie twice
- *  Optimize for runtime over memory
+ * Requirements:
+ *  - Users will watch exactly two movies
+ *  - Users cannot watch the same movie twice
+ *  - Optimize for runtime over memory
  */
 
 #[cfg(test)]
@@ -18,63 +18,63 @@ mod tests {
 
     // #[test]
     // fn short_flight() {
-    // let actual = two_movies(vec![2, 4], 1);
+    // let actual = two_movies(&[2, 4], 1);
     // let expected = false;
     // assert_eq!(actual, expected);
     // }
 
     // #[test]
     // fn long_flight() {
-    // let actual = two_movies(vec![2, 4], 6);
+    // let actual = two_movies(&[2, 4], 6);
     // let expected = true;
     // assert_eq!(actual, expected);
     // }
 
     // #[test]
     // fn one_movie_half_flight_length() {
-    // let actual = two_movies(vec![3, 8], 6);
+    // let actual = two_movies(&[3, 8], 6);
     // let expected = false;
     // assert_eq!(actual, expected);
     // }
 
     // #[test]
     // fn two_movies_half_flight_length() {
-    // let actual = two_movies(vec![3, 8, 3], 6);
+    // let actual = two_movies(&[3, 8, 3], 6);
     // let expected = true;
     // assert_eq!(actual, expected);
     // }
 
     // #[test]
     // fn lots_of_possible_pairs() {
-    // let actual = two_movies(vec![1, 2, 3, 4, 5, 6], 7);
+    // let actual = two_movies(&[1, 2, 3, 4, 5, 6], 7);
     // let expected = true;
     // assert_eq!(actual, expected);
     // }
 
     // #[test]
     // fn not_using_first_movie() {
-    // let actual = two_movies(vec![4, 3, 2], 5);
+    // let actual = two_movies(&[4, 3, 2], 5);
     // let expected = true;
     // assert_eq!(actual, expected);
     // }
 
     // #[test]
     // fn multiple_movies_shorter_than_flight() {
-    // let actual = two_movies(vec![5, 6, 7, 8], 9);
+    // let actual = two_movies(&[5, 6, 7, 8], 9);
     // let expected = false;
     // assert_eq!(actual, expected);
     // }
 
     // #[test]
     // fn only_one_movie() {
-    // let actual = two_movies(vec![6], 6);
+    // let actual = two_movies(&[6], 6);
     // let expected = false;
     // assert_eq!(actual, expected);
     // }
 
     // #[test]
     // fn no_movies() {
-    // let actual = two_movies(vec![], 2);
+    // let actual = two_movies(&[], 2);
     // let expected = false;
     // assert_eq!(actual, expected);
     // }
--- src/concepts_and_algorithms/hashing_and_hash_tables/two_movies/two_movies_solution.rs
+++ src/concepts_and_algorithms/hashing_and_hash_tables/two_movies/two_movies_solution.rs
@@ -6,24 +6,24 @@
  * movie_lengths (in minutes) and returns a boolean indicating whether there are two numbers in
  * movie_lengths whose sum equals flightLength.
  *
- * When building your function:
- *  Assume your users will watch exactly two movies
- *  Don't make your users watch the same movie twice
- *  Optimize for runtime over memory
+ * Requirements:
+ *  - Users will watch exactly two movies
+ *  - Users cannot watch the same movie twice
+ *  - Optimize for runtime over memory
  */
 
 use std::collections::HashSet;
 
-fn two_movies(movie_lengths: Vec<i32>, flight_length: i32) -> bool {
-    let mut movie_lengths_seen = HashSet::new();
+fn two_movies(movie_lengths: &[i32], flight_length: i32) -> bool {
+    let mut lengths_seen = HashSet::with_capacity(movie_lengths.len());
 
-    for first_movie_length in movie_lengths {
-        let matching_second_movie_length = flight_length - first_movie_length;
-        if movie_lengths_seen.contains(&matching_second_movie_length) {
+    for &movie_length in movie_lengths {
+        let matching_second_movie_length = flight_length - movie_length;
+        if lengths_seen.contains(&matching_second_movie_length) {
             return true;
         }
 
-        movie_lengths_seen.insert(first_movie_length);
+        lengths_seen.insert(movie_length);
     }
 
     false
@@ -35,63 +35,63 @@ mod tests {
 
     #[test]
     fn short_flight() {
-        let actual = two_movies(vec![2, 4], 1);
+        let actual = two_movies(&[2, 4], 1);
         let expected = false;
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn long_flight() {
-        let actual = two_movies(vec![2, 4], 6);
+        let actual = two_movies(&[2, 4], 6);
         let expected = true;
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn one_movie_half_flight_length() {
-        let actual = two_movies(vec![3, 8], 6);
+        let actual = two_movies(&[3, 8], 6);
         let expected = false;
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn two_movies_half_flight_length() {
-        let actual = two_movies(vec![3, 8, 3], 6);
+        let actual = two_movies(&[3, 8, 3], 6);
         let expected = true;
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn lots_of_possible_pairs() {
-        let actual = two_movies(vec![1, 2, 3, 4, 5, 6], 7);
+        let actual = two_movies(&[1, 2, 3, 4, 5, 6], 7);
         let expected = true;
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn not_using_first_movie() {
-        let actual = two_movies(vec![4, 3, 2], 5);
+        let actual = two_movies(&[4, 3, 2], 5);
         let expected = true;
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn multiple_movies_shorter_than_flight() {
-        let actual = two_movies(vec![5, 6, 7, 8], 9);
+        let actual = two_movies(&[5, 6, 7, 8], 9);
         let expected = false;
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn only_one_movie() {
-        let actual = two_movies(vec![6], 6);
+        let actual = two_movies(&[6], 6);
         let expected = false;
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn no_movies() {
-        let actual = two_movies(vec![], 2);
+        let actual = two_movies(&[], 2);
         let expected = false;
         assert_eq!(actual, expected);
     }
